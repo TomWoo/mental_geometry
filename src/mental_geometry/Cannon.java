af983f9dc6e2a6f9419d7237bbfad98374fcb477
TomWoo <tomw.82008@gmail.com>
2014-11-15 18:18:35 -0500

    test commit

--- src/mental_geometry/Cannon.java
+++ src/mental_geometry/Cannon.java
@@ -4,28 +4,53 @@ import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Cannon {
-	private int angle, targetAngle;
+	private double angle = 0, targetAngle = 0;
+	private double vRot = 1;
 	private int x, y;
-	private int vRot = 1;
 	private boolean readyToFire = false, visible = false;
 	BufferedImage image = null;
 
-	public void update() {
+	public Cannon(String filename, int x0, int y0) {
+		try {
+			image = ImageIO.read(new File(filename));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		x = x0;
+		y = y0;
+	}
+	
+	public void setTargetAngle(double targetAngle) {
+		this.targetAngle = targetAngle;
+	}
+
+	public double getTargetAngle() {
+		return targetAngle;
+	}
+	
+	public AffineTransformOp update() {
 		// Moves Character or Scrolls Background accordingly.
 		double rotationRequired = 0;
-		if(this.angle < targetAngle) {
-			this.angle += vRot;
+		if(angle < targetAngle) {
+			angle += vRot;
 			rotationRequired = Math.toRadians(vRot);
-		} else if(this.angle > targetAngle) {
-			this.angle -= vRot;
+		} else if(angle > targetAngle) {
+			angle -= vRot;
 			rotationRequired = -Math.toRadians(vRot);
 		}
+		System.out.println(angle+" "+targetAngle+" "+rotationRequired);
 		AffineTransform tx = AffineTransform.getRotateInstance(
 				rotationRequired, x, y);
 		AffineTransformOp op = new AffineTransformOp(tx,
 				AffineTransformOp.TYPE_BILINEAR);
+		return op;
 //		Graphics2D g2d = (Graphics2D) g;
 //		g2d.drawImage(op.filter(image, null), x, y, null);
 	}
@@ -45,8 +70,12 @@ public class Cannon {
 		}
 	}
 	
-	public int getAngle() {
+	public double getAngle() {
 		return angle;
+	}
+	
+	public void setAngle(double angle) {
+		this.angle = angle;
 	}
 
 	public int getX() {
--- src/mental_geometry/GameStart.java
+++ src/mental_geometry/GameStart.java
@@ -3,10 +3,16 @@ package mental_geometry;
 import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.AffineTransformOp;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+
+import javax.imageio.ImageIO;
 
 public class GameStart extends Applet implements Runnable, KeyListener{
 	
@@ -17,12 +23,13 @@ public class GameStart extends Applet implements Runnable, KeyListener{
 	GameState state = GameState.Running;
 	private Image background1;
 	private URL base;
-	private Image image, cannonballIm, targetIm ;
+	private Image image, cannonballIm, targetIm;
 	private Graphics gr;
 	private Ball cannonball;
 	public static Ball target;
 	public static boolean levelWon;
 	
+	private Cannon cannon;
 	
 	@Override
 	public void init(){
@@ -37,11 +44,14 @@ public class GameStart extends Applet implements Runnable, KeyListener{
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
-        background1 = getImage(base, "images/b1.png");
-        cannonballIm = getImage(base, "images/cannonball.png"); 
-        targetIm = getImage(base,"images/target.png");
-        
-        
+        try {
+			background1 = ImageIO.read(new File("images/b1.png"));
+	        cannonballIm = ImageIO.read(new File("images/ball.png")); 
+	        targetIm = ImageIO.read(new File("images/target.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
@@ -49,6 +59,9 @@ public class GameStart extends Applet implements Runnable, KeyListener{
 		cannonball = new Ball(0,0,1,1, false, cannonballIm); //CHANGE THIS!!!
 		target = new Ball(0,0,1,1, true, targetIm); //CHANGE THIS!!!
 		levelWon = false;
+        
+        cannon = new Cannon("images/cannon-new.png", 200, 200); //change this
+        cannon.setTargetAngle(180);
 		
 		Thread thread = new Thread(this);
 	    thread.start();
@@ -63,7 +76,8 @@ public class GameStart extends Applet implements Runnable, KeyListener{
 	public void destroy(){
 		super.destroy();
 	}
-	
+
+	int counter = 0;
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
@@ -73,9 +87,8 @@ public class GameStart extends Applet implements Runnable, KeyListener{
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		
+		System.out.print("run"+counter+" ");
 	}
-	
 	@Override
 	public void update(Graphics g){
 		if (image == null) {
@@ -89,11 +102,19 @@ public class GameStart extends Applet implements Runnable, KeyListener{
 		paint(gr);
 
 		g.drawImage(image, 0, 0, this);
+		System.out.print("update"+counter+" ");
 	}
 	
 	@Override
 	public void paint(Graphics g){
 		g.drawImage(background1,0,0,this);
+		
+//		g.drawImage(cannon.getImage(), cannon.getX(), cannon.getY(), this);
+//		AffineTransformOp op = cannon.update();
+//		Graphics2D g2d = (Graphics2D) g;
+//		g2d.drawImage(op.filter(cannon.getImage(), null), cannon.getX(), cannon.getY(), null);
+		System.out.println("paint"+counter+" ");
+		counter++;
 	}
 	
 	@Override
