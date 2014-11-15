faa4f053d3b149f64e11896ff017359b84646f67
Joy Patel <joypatel100@yahoo.com>
2014-11-15 18:28:59 -0500

    Updated GameStart to show images

--- src/mental_geometry/Shapes.java
+++ src/mental_geometry/Shapes.java
@@ -4,19 +4,76 @@ import java.awt.Image;
 
 public class Shapes {
 	int cannonX, cannonY, targetX, targetY;
-	double angle;
+	double soluAngle, cannonAngle;
 	Image shapeIm;
 	
-	public Shapes(int cX, int cY, int tX, int tY, double a, Image sIm){
+	public Shapes(int cX, int cY, int tX, int tY, double ca, double sa, Image sIm){
 		this.cannonX = cX;
 		this.cannonY = cY;
 		this.targetX = tX;
 		this.targetY = tY;
-		this.angle = a;
+		this.cannonAngle = ca;
+		this.soluAngle = sa;
 		this.shapeIm = sIm;
 	}
 	
 	private double solve(double offset){
-		return offset + angle;
+		return offset + soluAngle - cannonAngle;
+	}
+
+	public int getCannonX() {
+		return cannonX;
+	}
+
+	public void setCannonX(int cannonX) {
+		this.cannonX = cannonX;
+	}
+
+	public int getCannonY() {
+		return cannonY;
+	}
+
+	public void setCannonY(int cannonY) {
+		this.cannonY = cannonY;
+	}
+
+	public int getTargetX() {
+		return targetX;
+	}
+
+	public void setTargetX(int targetX) {
+		this.targetX = targetX;
+	}
+
+	public int getTargetY() {
+		return targetY;
+	}
+
+	public void setTargetY(int targetY) {
+		this.targetY = targetY;
+	}
+
+	public double getSoluAngle() {
+		return soluAngle;
+	}
+
+	public void setSoluAngle(double soluAngle) {
+		this.soluAngle = soluAngle;
+	}
+
+	public double getCannonAngle() {
+		return cannonAngle;
+	}
+
+	public void setCannonAngle(double cannonAngle) {
+		this.cannonAngle = cannonAngle;
+	}
+
+	public Image getShapeIm() {
+		return shapeIm;
+	}
+
+	public void setShapeIm(Image shapeIm) {
+		this.shapeIm = shapeIm;
 	}
 }
